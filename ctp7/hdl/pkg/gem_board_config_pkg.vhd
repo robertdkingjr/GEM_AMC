adf03ee755af4b0fd3302fa839648dbf31ccfa2f
Evaldas Juska <evka85@gmail.com>
2016-05-14 03:04:06 +0200

    v1.3.1 -- has 12 OHs on all tree CXPs

--- ctp7/hdl/pkg/gem_board_config_pkg.vhd
+++ ctp7/hdl/pkg/gem_board_config_pkg.vhd
@@ -20,7 +20,7 @@ use IEEE.STD_LOGIC_1164.all;
 package gem_board_config_package is
 
     --constant CFG_NUM_OF_OHs     : integer := 2;
-    constant CFG_NUM_OF_OHs     : integer := 4;
+    constant CFG_NUM_OF_OHs     : integer := 12;
 
 
     --========================--
@@ -43,7 +43,17 @@ package gem_board_config_package is
         (0, 0, 1, 2), 
         (3, 3, 4, 5), 
         (6, 6, 7, 8), 
-        (9, 9, 10, 11) 
+        (9, 9, 10, 11),
+
+        (12, 12, 13, 14), 
+        (15, 15, 16, 17), 
+        (18, 18, 19, 20), 
+        (21, 21, 22, 23), 
+
+        (24, 24, 25, 26), 
+        (27, 27, 28, 29), 
+        (30, 30, 31, 32), 
+        (33, 33, 34, 35) 
 	);
 
 end package gem_board_config_package;
