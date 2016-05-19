085bb7407580b4b446e3a76e516673378847d26d
robertdkingjr <robertdkingjr@gmail.com>
2016-05-19 14:04:59 -0500

    Prettier printout, masking implemented

--- ctp7/apps/reg_interface/rw_reg.py
+++ ctp7/apps/reg_interface/rw_reg.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as xml
+import sys, os, subprocess
 
-ADDRESS_TABLE_TOP = '../../../scripts/address_table/gem_amc_top.xml'
+ADDRESS_TABLE_TOP = '/mnt/persistent/texas/gem_amc_top.xml'
+nodes = []
 
 class Node:
     name = ''
@@ -21,13 +23,7 @@ class Node:
         return self.name.replace(TOP_NODE_NAME + '.', '').replace('.', '_')
 
 def main():
-    tree = xml.parse(ADDRESS_TABLE_TOP)
-    root = tree.getroot()[0]
-
-    nodes = []
-    vars = {}
-    
-    makeTree(root,'',0x0,nodes,None,vars,False)
+    configure()
     print 'Example:'
     random_node = nodes[76]
     #print str(random_node.__class__.__name__)
@@ -37,6 +33,11 @@ def main():
     getAllChildren(random_node, kids)
     print len(kids), kids.name
 
+def parseXML():
+    tree = xml.parse(ADDRESS_TABLE_TOP)
+    root = tree.getroot()[0]
+    vars = {}
+    makeTree(root,'',0x0,nodes,None,vars,False)
 
 def makeTree(node,baseName,baseAddress,nodes,parentNode,vars,isGenerated):
     
@@ -49,7 +50,6 @@ def makeTree(node,baseName,baseAddress,nodes,parentNode,vars,isGenerated):
             #print('generate base_addr = ' + hex(baseAddress + generateAddressStep * i) + ' for node ' + node.get('id'))
             makeTree(node, baseName, baseAddress + generateAddressStep * i, nodes, parentNode, vars, True)
         return
-
 
     newNode = Node()
     name = baseName
@@ -89,7 +89,62 @@ def getAllChildren(node,kids=[]):
         for child in node.children:
             getAllChildren(child,kids)
 
+def getNode(nodeName):
+    nodelist = [node for node in nodes if node.name == nodeName]
+    if len(nodelist): return nodelist[0]
+    else: return None
 
+def getNodesContaining(nodeString):
+    nodelist = [node for node in nodes if nodeString in node.name]
+    if len(nodelist): return nodelist
+    else: return None
+
+def readAddress(address):
+    try: 
+        output = subprocess.check_output('mpeek '+str(hex(address)), stderr=subprocess.STDOUT , shell=True)
+        value = ''.join(s for s in output if s.isalnum())
+    except subprocess.CalledProcessError as e: value = parseError(int(str(e)[-1:]))
+    return value
+
+def readRawAddress(raw_address):
+    address = raw_address
+    address = address << 2
+    address = address + 0x64000000
+    return readAddress(address)
+
+def readReg(reg):
+    address = reg.address
+    address = address << 2
+    address = address + 0x64000000
+    # mpeek <address>
+    try: 
+        output = subprocess.check_output('mpeek '+str(address), stderr=subprocess.STDOUT , shell=True)
+        value = ''.join(s for s in output if s.isalnum())
+    except subprocess.CalledProcessError as e: return parseError(int(str(e)[-1:]))
+    # Apply Mask
+    if reg.mask is not None:
+        shift_amount=0
+        for bit in reversed('{0:b}'.format(reg.mask)):
+            if bit=='0': shift_amount+=1
+            else: break
+        final_value = (parseInt(str(reg.mask))&parseInt(value)) >> shift_amount
+    else:
+        final_value = value
+    final_int =  parseInt(str(final_value))
+    return '{0:#010x}'.format(final_int)
+
+def isValid(address):
+    try: subprocess.check_output('mpeek '+str(hex(address)), stderr=subprocess.STDOUT , shell=True)
+    except subprocess.CalledProcessError as e: return False
+    return True
+
+def parseError(e):
+    if e==1:
+        return "Failed to parse address"
+    if e==2:
+        return "Bus error"
+    else:
+        return "Unknown error: "+str(e)
 
 def parseInt(string):
     if string is None:
@@ -102,7 +157,6 @@ def parseInt(string):
         return int(string)
 
 
-     
 def substituteVars(string, vars):
     if string is None:
         return string
@@ -111,6 +165,8 @@ def substituteVars(string, vars):
         ret = ret.replace('${' + varKey + '}', str(vars[varKey]))
     return ret
 
+def tabPad(s,maxlen):
+    return s+"\t"*((maxlen-len(s)-1)/8+1)
 
 if __name__ == '__main__':
     main()
