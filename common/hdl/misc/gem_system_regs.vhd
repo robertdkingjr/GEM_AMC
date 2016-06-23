cd3f85df07ea663504811fe391f0c5e6904a9d7c
Evaldas Juska <evka85@gmail.com>
2016-06-24 01:40:29 +0200

    v1.4.0 -- Remapped CTP7 TX and RX links to fibers (going in sequnce)

--- common/hdl/misc/gem_system_regs.vhd
+++ common/hdl/misc/gem_system_regs.vhd
@@ -59,8 +59,8 @@ architecture gem_system_regs_arch of gem_system_regs is
     --== LEGACY Firmware date and version (taken from GLIB) ==--
     -- TODO: remove legacy firmware date and version once the software is ready 
     constant c_legacy_sys_ver_year :integer range 0 to 99 :=16;
-    constant c_legacy_sys_ver_month:integer range 0 to 12 :=5;
-    constant c_legacy_sys_ver_day  :integer range 0 to 31 :=26;
+    constant c_legacy_sys_ver_month:integer range 0 to 12 :=6;
+    constant c_legacy_sys_ver_day  :integer range 0 to 31 :=23;
     constant c_legacy_board_id     : std_logic_vector(31 downto 0) := x"474c4942"; -- GLIB
     constant c_legacy_sys_id       : std_logic_vector(31 downto 0) := x"32307631"; -- '2_0_v1'
     
--- ctp7/hdl/pkg/gem_board_config_pkg.vhd
+++ ctp7/hdl/pkg/gem_board_config_pkg.vhd
@@ -58,6 +58,55 @@ package gem_board_config_package is
 --        (33, 33, 34, 35, 34, 35) 
 	);
 
+    -- this record is used in CXP fiber to GTH map (holding tx and rx GTH index)
+    type t_cxp_fiber_to_gth_link is record
+        tx      : integer range 0 to 35; -- GTH TX index
+        rx      : integer range 0 to 35; -- GTH RX index
+    end record;
+    
+    -- this array is meant to hold mapping from CXP fiber index to GTH TX and RX indexes
+    type t_cxp_fiber_to_gth_link_map is array (0 to 35) of t_cxp_fiber_to_gth_link;
+
+    -- defines the GTH TX and RX index for each index of the CXP fiber
+    constant CFG_CXP_FIBER_TO_GTH_MAP : t_cxp_fiber_to_gth_link_map := (
+        (1, 2), -- fiber 0 (CXP 0)
+        (3, 0), -- fiber 1
+        (5, 4), -- fiber 2
+        (0, 3), -- fiber 3
+        (2, 5), -- fiber 4
+        (4, 1), -- fiber 5
+        (10, 7), -- fiber 6
+        (8, 9), -- fiber 7
+        (6, 10), -- fiber 8
+        (11, 6), -- fiber 9
+        (9, 8), -- fiber 10
+        (7, 11), -- fiber 11
+        (13, 15), -- fiber 12 (CXP 1)
+        (15, 12), -- fiber 13
+        (17, 16), -- fiber 14
+        (12, 14), -- fiber 15 
+        (14, 18), -- fiber 16
+        (16, 13), -- fiber 17
+        (22, 19), -- fiber 18
+        (20, 23), -- fiber 19
+        (18, 20), -- fiber 20
+        (23, 17), -- fiber 21
+        (21, 21), -- fiber 22
+        (19, 22), -- fiber 23
+        (25, 27), -- fiber 24 (CXP 2)
+        (27, 24), -- fiber 25
+        (29, 28), -- fiber 26
+        (24, 26), -- fiber 27
+        (26, 30), -- fiber 28
+        (28, 25), -- fiber 29
+        (34, 31), -- fiber 30
+        (32, 35), -- fiber 31
+        (30, 32), -- fiber 32
+        (35, 29), -- fiber 33
+        (33, 33), -- fiber 34
+        (31, 34)  -- fiber 35
+    );
+    
 end package gem_board_config_package;
 --============================================================================
 --                                                                 Package end 
