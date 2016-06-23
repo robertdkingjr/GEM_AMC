e48e7012f0a71b26625a03cf646c2862a5735edb
Robert King <kingr@gem904daq01.cern.ch>
2016-06-23 18:59:13 +0200

    Several commands added

--- ctp7/apps/reg_interface/reg_interface.py
+++ ctp7/apps/reg_interface/reg_interface.py
@@ -2,17 +2,118 @@ from cmd import Cmd
 import sys, os, subprocess
 from rw_reg import *
 
+MAX_OH_NUM = 3
 
 
 class Prompt(Cmd):
 
-    def do_hello(self, args):
-        """Says hello. If you provide a name, it will greet you with it."""
-        if len(args) == 0:
-            name = 'stranger'
+
+    def do_test(self, args):
+        print 'Test here!'
+        print 'args:',args
+
+    def do_oh(self, args):
+        """ Begin command by selecting OH, followed by command. USAGE oh <number> <command> """
+        arglist = args.split()
+        if len(arglist)<1: print 'Too few arguments.'
+        if len(arglist)==1:
+            if not arglist[0].isdigit():
+                print 'Invalid OH number:',arglist[0]
+                return
+            elif int(arglist[0])<0 or int(arglist[0])>MAX_OH_NUM: print 'Invalid OH number:',arglist[0]
+            else:
+                if getNodesContaining('GEM_AMC.OH.OH'+str(arglist[0])) is not None:
+                    for reg in getNodesContaining('GEM_AMC.OH.OH'+str(arglist[0])):
+                        address = reg.real_address
+                        if 'r' in str(reg.permission):
+                            print hex(address).rstrip('L'),reg.permission,'\t',tabPad(reg.name,7),readReg(reg)
+                else: print 'Regs not found!'
+
+        elif not arglist[0].isdigit(): print 'Incorrect usage.'
+        elif int(arglist[0])<0 or int(arglist[0])>MAX_OH_NUM: print 'Invalid OH number:',arglist[0]
         else:
-            name = args
-        print "Hello, %s" % name
+            new_args=''
+            for i in range(2,len(arglist)):
+                new_args += arglist[i]+' '
+            if arglist[1]=='v2a': self.do_v2a(arglist[0])
+            elif arglist[1]=='test': self.do_test(new_args)
+            elif arglist[1]=='mask': self.do_mask(arglist[0]+' '+new_args)
+            #elif arglist[1]=='unmask': self.do_unmask(arglist[0]+' '+new_args)
+            else: print 'No command found:',arglist[1]
+
+
+    def do_daq(self, args):
+        """Read all registers in DAQ module. USAGE: daq <optional OH_NUM>"""
+        arglist = args.split()
+        if len(arglist)==1: 
+            if not arglist[0].isdigit(): 
+                print 'Incorrect usage.'
+                return
+            elif int(arglist[0])<0 or int(arglist[0])>MAX_OH_NUM: print 'Invalid OH number:',arglist[0]
+            else:                
+                if getNodesContaining('GEM_AMC.DAQ.OH'+str(arglist[0])) is not None:
+                    for reg in getNodesContaining('GEM_AMC.DAQ.OH'+str(arglist[0])):
+                        address = reg.real_address
+                        if 'r' in str(reg.permission):
+                            print hex(address).rstrip('L'),reg.permission,'\t',tabPad(reg.name,7),readReg(reg)
+                else: print 'Regs not found!'
+        
+        elif args=='': 
+            if getNodesContaining('GEM_AMC.DAQ') is not None:
+                for reg in getNodesContaining('GEM_AMC.DAQ'):
+                    address = reg.real_address
+                    if 'r' in str(reg.permission):
+                        print hex(address).rstrip('L'),reg.permission,'\t',tabPad(reg.name,7),readReg(reg)
+            else: print 'Regs not found!'
+        
+        else: print 'Incorrect usage.'
+
+    def do_gemsystem(self, args):
+        """Read all registers in GEM_SYSTEM module. USAGE: gemsystem"""
+        if args == '':
+            if getNodesContaining('GEM_AMC.GEM_SYSTEM') is not None:
+                for reg in getNodesContaining('GEM_AMC.GEM_SYSTEM'):
+                    address = reg.real_address
+                    if 'r' in str(reg.permission):
+                        print hex(address).rstrip('L'),reg.permission,'\t',tabPad(reg.name,7),readReg(reg)
+            else: print 'Regs not found!'
+
+    def do_ttc(self, args):
+        """Read all registers in TTC module. USAGE: ttc"""
+        if args == '':
+            if getNodesContaining('GEM_AMC.TTC') is not None:
+                for reg in getNodesContaining('GEM_AMC.GEM_SYSTEM'):
+                    address = reg.real_address
+                    if 'r' in str(reg.permission):
+                        print hex(address).rstrip('L'),reg.permission,'\t',tabPad(reg.name,7),readReg(reg)
+            else: print 'Regs not found!'
+
+    def do_trigger(self, args):
+        """Read all registers in TRIGGER module. USAGE: trigger <optional OH_NUM>"""
+        arglist = args.split()
+        if len(arglist)==1:
+            if not arglist[0].isdigit():
+                print 'Incorrect usage.'
+                return
+            elif int(arglist[0])<0 or int(arglist[0])>MAX_OH_NUM: print 'Invalid OH number:',arglist[0]
+            else:
+                if getNodesContaining('GEM_AMC.TRIGGER.OH'+str(arglist[0])) is not None:
+                    for reg in getNodesContaining('GEM_AMC.TRIGGER.OH'+str(arglist[0])):
+                        address = reg.real_address
+                        if 'r' in str(reg.permission):
+                            print hex(address).rstrip('L'),reg.permission,'\t',tabPad(reg.name,7),readReg(reg)
+                else: print 'Regs not found!'
+        elif args=='':
+            if getNodesContaining('GEM_AMC.TRIGGER') is not None:
+                for reg in getNodesContaining('GEM_AMC.TRIGGER'):
+                    address = reg.real_address
+                    if 'r' in str(reg.permission):
+                        print hex(address).rstrip('L'),reg.permission,'\t',tabPad(reg.name,7),readReg(reg)
+            else: print 'Regs not found!'
+
+        else: print 'Incorrect usage.'
+
+
 
 
     def do_read(self, args):
@@ -144,11 +245,83 @@ class Prompt(Cmd):
         else: print "Incorrect number of arguments!"
 
     def do_debug(self,args):
-        """Quick read of SBit Clusters"""
-        for reg in getNodesContaining('OH'+args+'.DEBUG_LAST'):
-            if 'r' in str(reg.permission): print hex(reg.real_address),reg.permission,'\t',tabPad(reg.name,4),readReg(reg)
+        """Quick read of SBit Clusters. USAGE: debug <OH_number>"""
+        arglist = args.split()
+        if len(arglist)==1:
+            for reg in getNodesContaining('OH'+str(args)+'.DEBUG_LAST'):
+                if 'r' in str(reg.permission): print hex(reg.real_address),reg.permission,'\t',tabPad(reg.name,4),readReg(reg)
+        else: print "Incorrect number of arguments!"
 
+
+    def do_v2a(self,args):
+        """Configure recovered clock for OHv2a"""
+        arglist = args.split()
+        if len(arglist)==1:
+            if not args.isdigit() or int(args)<0 or int(args)>2: 
+                print 'Invalid OH number.'
+                return
+            reg = getNode('GEM_AMC.OH.OH'+str(args)+'.CONTROL.CLOCK.REF_CLK')
+            if reg is not None:
+                try: print writeReg(reg,1)
+                except:
+                    print 'Write error.'
+                    return
+            else: print 'Error finding clock control register!'
+        else: print "Incorrect number of arguments!"
+
+
+    def do_mask(self, args):
+        """Mask single VFAT to data. If no VFAT provided, will mask all VFATs. USAGE mask <OH_NUM> <optional VFAT_SLOT> """
+        arglist = args.split()
+        if len(arglist)==1 and isValidOH(arglist[0]):
+            print writeReg(getNode('GEM_AMC.OH.OH'+str(arglist[0])+'.CONTROL.VFAT.MASK'),0xffffffff)
+        elif len(arglist)==2 and isValidOH(arglist[0]) and isValidVFAT(arglist[1]):
+            vfat = int(arglist[1])
+            oh = int(arglist[0])
+            mask = (0x1 << vfat)
+            try: current_mask = parseInt(readReg(getNode('GEM_AMC.OH.OH'+str(oh)+'.CONTROL.VFAT.MASK')))
+            except: 
+                print 'Error reading current mask.'
+                return
+            new_mask = mask | current_mask
+            print writeReg(getNode('GEM_AMC.OH.OH'+str(oh)+'.CONTROL.VFAT.MASK'),new_mask)
+        else:
+            print 'Incorrect usage.'
+            return
+                                       
+    def do_unmask(self, args):
+        """Unmask single VFAT to data. If no VFAT provided, will unmask all VFATs. USAGE unmask <OH_NUM> <optional VFAT_SLOT> """
+        arglist = args.split()
+        if len(arglist)==1 and isValidOH(arglist[0]):
+            print writeReg(getNode('GEM_AMC.OH.OH'+str(arglist[0])+'.CONTROL.VFAT.MASK'),0x00000000)
+        elif len(arglist)==2 and isValidOH(arglist[0]) and isValidVFAT(arglist[1]):
+            vfat = int(arglist[1])
+            oh = int(arglist[0])
+            mask = 0xffffffff ^ (0x1 << vfat)
+            try: current_mask = parseInt(readReg(getNode('GEM_AMC.OH.OH'+str(oh)+'.CONTROL.VFAT.MASK')))
+            except:
+                print 'Error reading current mask.'
+                return
+            new_mask = mask & current_mask
+            print writeReg(getNode('GEM_AMC.OH.OH'+str(oh)+'.CONTROL.VFAT.MASK'),new_mask)
+        else:
+            print 'Incorrect usage.'
+            return
+
+        
+
+        
+def isValidOH(oh):
+    if not oh.isdigit(): return False
+    if int(oh)<0 or int(oh)>MAX_OH_NUM: return False
+    return True
     
+def isValidVFAT(vfat):
+    if not vfat.isdigit(): return False
+    if int(vfat)<0 or int(vfat)>23: return False
+    return True
+
+
 
 if __name__ == '__main__':
     try:
@@ -156,5 +329,5 @@ if __name__ == '__main__':
         prompt = Prompt()
         prompt.prompt = 'CTP7 > '
         prompt.cmdloop('Starting CTP7 Register Command Line Interface.')
-    except:
+    except KeyboardInterrupt:
         print '\n'
