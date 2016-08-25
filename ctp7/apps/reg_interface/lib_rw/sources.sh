61b2003a4d4639bbc1021b51157d351b5b19853c
Robert King <robertdkingjr@gmail.com>
2016-08-25 02:17:55 +0200

    Updated timer - still doesn't work

--- ctp7/apps/reg_interface/lib_rw/sources.sh
+++ ctp7/apps/reg_interface/lib_rw/sources.sh
@@ -1,4 +1,4 @@
 source /opt/Xilinx/Vivado/2016.1/settings64.sh
 source /data/bigdisk/sw/Xilinx/SDK/2016.2/settings64.sh
-export STAGEDIR=../../stage
-export PETA_STAGE=../../stage
+export STAGEDIR=../../apps/stage
+export PETA_STAGE=../../apps/stage
--- ctp7/apps/reg_interface/timing.cxx
+++ ctp7/apps/reg_interface/timing.cxx
@@ -1,28 +1,28 @@
 #include <stdio.h>      /* printf */
-#include <time.h>
+// #include <time.h>
 #include <rwreg.h>
-#include <string.h>
+// #include <string.h>
 
-using namespace std;
+// using namespace std;
 
 int main()
 {
-  clock_t t;
-  string ctp7_reg = "GEM_AMC.GEM_SYSTEM.BOARD_ID";
-  unsigned int ctp7_address = 0x66400008;
-  string oh_reg = "";
-  string glib_reg = "GEM_AMC.GEM_SYSTEM.BOARD_ID";
-  unsigned int glib_address = 0;
+  // clock_t t;
+  // string ctp7_reg = "GEM_AMC.GEM_SYSTEM.BOARD_ID";
+  // unsigned int ctp7_address = 0x66400008;
+  // string oh_reg = "";
+  // string glib_reg = "GEM_AMC.GEM_SYSTEM.BOARD_ID";
+  // unsigned int glib_address = 0;
   
-  t = clock();
-  for (int i = 0;i<1000;i++) {
-    getReg(ctp7_address);
-  }
-  t = clock() - t;
+  // t = clock();
+  // for (int i = 0;i<1000;i++) {
+  //   getReg(ctp7_address);
+  // }
+  // t = clock() - t;
 
-  printf("Time to read 1000 registers: %d clicks, %f seconds\n",t,((float)t/CLOCKS_PER_SEC));
+  // printf("Time to read 1000 registers: %d clicks, %f seconds\n",t,((float)t/CLOCKS_PER_SEC));
 
-
+  printf("Hello");
 
 
 }
